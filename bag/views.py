e6d4afec2a97ae1aa3dc950bcd2253020e7e0152
NickSpriggs <ntspriggs@me.com>
2022-01-09 18:05:46 +0000

    updated context processor

--- bag/views.py
+++ bag/views.py
@@ -3,13 +3,12 @@ from django.shortcuts import render, redirect
 # Create your views here.
 
 def view_bag(request):
-    """
-    A view to return the bag contents page
-    """
+    """ A view that renders the bag contents page """
+
     return render(request, 'bag/bag.html')
 
 def add_to_bag(request, item_id):
-    """Add a quantity off the specified product to the shopping bag"""
+    """ Add a quantity of the specified product to the shopping bag """
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
@@ -19,7 +18,7 @@ def add_to_bag(request, item_id):
         bag[item_id] += quantity
     else:
         bag[item_id] = quantity
-    
+
     request.session['bag'] = bag
-    print(request.session['bag'])
     return redirect(redirect_url)
+    
